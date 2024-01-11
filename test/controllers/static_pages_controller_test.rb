4b899d9b594f82fb6e3a5f8290db33c36d2ff2ed
cbstodd <cbstodd@gmail.com>
2024-01-10 19:51:57 -0500

    Added dynamic titles to the Static Pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,26 +4,31 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    assert_select "title", "Home | Sky💫Cab"
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
+    assert_select "title", "Help | Sky💫Cab"
   end
 
   test "should get terms_and_conditions" do
     get static_pages_terms_and_conditions_url
     assert_response :success
+    assert_select "title", "Terms & Conditions | Sky💫Cab"
   end
 
   test "should get privacy_policy" do
     get static_pages_privacy_policy_url
     assert_response :success
+    assert_select "title", "Privacy Policy | Sky💫Cab"
   end
 
   test "should get FAQ page" do
     get static_pages_faq_url
     assert_response :success
+    assert_select "title", "Questions | Sky💫Cab"
   end
 
 end
